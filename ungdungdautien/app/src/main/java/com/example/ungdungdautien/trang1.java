e87916694449b7e173eabf0f3476dee4093d9644
Bùi Thị Thêu <92207223+BUITHEU698@users.noreply.github.com>
2022-06-06 01:23:27 +0700

    1234

---
+++ ungdungdautien/app/src/main/java/com/example/ungdungdautien/trang1.java
@@ -0,0 +1,14 @@
+package com.example.ungdungdautien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trang1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang1);
+    }
+}
